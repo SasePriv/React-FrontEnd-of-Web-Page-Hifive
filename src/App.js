de5bd3dba3457890d0191aac59d1ef1a68ecfd3e
Luis Sanchez <sase.priv@gmail.com>
2020-02-08 16:49:21 -0400

    terminando el home con bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home';
-import Bar from './components/Bar';
 
 function App() {
   return (
     <div>
       <Home></Home>
-      {/* <Bar></Bar> */}
     </div>
   );
 }
--- src/components/Bar.js
+++ src/components/Bar.js
@@ -2,11 +2,41 @@ import React, {Component} from 'react';
 import './styles/bar.css';
 
 class Bar extends Component{
+    constructor(){
+        super();
+        this.state = {
+            auth: true
+        }
+    }
+
+    notificaction(){
+        if (this.state.auth) {
+            return (<span className="circule"></span>);
+        }
+    }
 
     render(){
+
         return(
             <div className="bar ">
-                p
+                <div className="d-flex justify-content-center prueba">
+                    <div  className="mr-5">
+                        <a href="#">
+                            <img src={require("../assets/img/menu1.png")}></img>
+                        </a>
+                    </div>
+                    <div  className="ml-2 mr-2" >
+                        <a href="#">
+                            <img src={require("../assets/img/Vector.png")}></img>
+                            {this.notificaction()}                            
+                        </a>                        
+                    </div>
+                    <div className="ml-5 perso">
+                        <a href="#">
+                            <img src={require("../assets/img/menu3.png")}></img>
+                        </a>                        
+                    </div>
+                </div>
             </div>
         )
     }
